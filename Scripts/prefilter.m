bc77ae677f1a5883d1587dde7aaf3c7639f38d51
Kenny Yau <kenny.yau@tufts.edu>
2018-02-07 17:21:01 -0500

    adds FFT graphing to aid in filter analysis

--- Scripts/prefilter.m
+++ Scripts/prefilter.m
@@ -10,8 +10,15 @@ cd(pathname)
 load('1002_Traces_Full.mat')
 
 plot(trace(:,2))
+N = 2.^nextpow2(length(trace(:,2)));
 
-
+f = (-N/2:N/2-1)*(Fs/N);
+TRACE = fftshift(fft(trace(:,2),N));
+plot(f,abs(TRACE))
+title('FFT (Absolute Value)')
+xlabel('Frequency (Hz)')
+ylabel('Magnitude')
+xlim([0 100])
 
 
 cd(origPath)
