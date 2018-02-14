620a25a980b6b56b157c6b87ba29f0b79e2e1fdc
Kenny <kenny.yau@tufts.edu>
2018-02-14 13:08:12 -0500

    adds initial plots.m program structure

---
+++ Scripts/plots.m
@@ -0,0 +1,33 @@
+%% Analysis Plots
+% Throws some plots onto the screen that may or may not be useful to the
+% typical neuroscientist
+
+% Plots include: (will be populated as I implement them)
+
+% Assumes that the correct trace windows has already been loaded into the
+% workspace.
+
+% FFT
+figure(1)
+N = 2.^nextpow2(length(trace_window));
+freqz(trace_window,N)
+title('FFT using freqz')
+
+figure(2)
+f = (-N/2:N/2-1)*(Fs/N);
+T = mag2db(abs(fftshift(fft(trace_window,N))))/2;
+plot(f(N/2:end),T(N/2:end))
+title('FFT graph')
+xlabel('Frequency (Hz)')
+ylabel('dB')
+xlim([0 Fs/2])
+
+% Spectrum Analyzer
+SpecAnalyze = dsp.SpectrumAnalyzer('SampleRate',Fs,'PlotAsTwoSidedSpectrum',false);
+SpecAnalyze(trace_window)
+
+% Spectrogram
+% hannwin = hann(64);
+figure(3)
+spectrogram(trace_window) % ,hannwin,64,'yaxis',N)
+title('Spectrogram')
