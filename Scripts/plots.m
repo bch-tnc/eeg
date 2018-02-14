60a72a438ccde3ab457a89aff2dc99615760369a
Kenny <kenny.yau@tufts.edu>
2018-02-14 17:14:47 -0500

    adds preliminary power ratio plot

--- Scripts/plots.m
+++ Scripts/plots.m
@@ -7,30 +7,68 @@
 % Assumes that the correct trace windows has already been loaded into the
 % workspace.
 
-% FFT
-figure(1)
-N = 2.^nextpow2(length(trace_window));
-freqz(trace_window,N)
-title('FFT using freqz')
+%% FFT
+% figure(1)
+% N = 2.^nextpow2(length(trace_window));
+% freqz(trace_window,N)
+% title('FFT using freqz')
 
 figure(2)
 f = (-N/2:N/2-1)*(Fs/N);
-T = mag2db(abs(fftshift(fft(trace_window,N))))/2;
+T = abs(fftshift(fft(trace_window,N)));
 plot(f(N/2:end),T(N/2:end))
 title('FFT graph')
 xlabel('Frequency (Hz)')
-ylabel('dB')
+ylabel('Magnitude')
 xlim([0 Fs/2])
+grid on
 
 % % Spectrum Analyzer
 % SpecAnalyze = dsp.SpectrumAnalyzer('SampleRate',Fs,'PlotAsTwoSidedSpectrum',false);
 % SpecAnalyze(trace_window)
 
-% Spectrogram
+%% Spectrogram
 M = 64;
 hannwin = hann(M);
 overlapPercent = 50;
 L = M*(overlapPercent/100);
 figure(3)
-spectrogram(trace_window,hannwin,L,N,'yaxis')
+spectrogram(trace_window,hannwin,L,N,Fs,'yaxis')
 title('Spectrogram')
+
+%% Power Bands Plot
+bands = [0.5  4     % delta
+         4    8     % theta
+         8   13     % alpha
+        13   30     % beta
+        30   80];   % gamma
+
+dim = size(bands);
+numBands = dim(1);
+bandsPower = zeros(1,numBands);
+
+for i = 1:numBands
+    currBand = bands(i,:);
+    lowBound = currBand(1);
+    upBound  = currBand(2);
+    
+    tempVec = T;
+    tempVec = tempVec(tempVec>=lowBound);
+    tempVec = tempVec(tempVec<upBound);
+    
+    meanPower = mean(sum(tempVec));
+    bandsPower(i) = meanPower;
+end
+
+T2 = T;
+T2 = T2(T2>=bands(1,1));
+T2 = T2(T2<bands(dim(1),dim(2)));
+totalPower = sum(T2);
+
+bandsPowerRatio = bandsPower/totalPower;
+
+figure
+stem(bandsPowerRatio)
+title('Power Ratio of Various Oscillation Bands')
+    
+    
