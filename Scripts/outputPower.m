6de7eba3a5a5451010a389c7cf408697afcb303d
Kenny <kenny.yau@tufts.edu>
2018-02-21 17:06:54 -0500

    changed goal to write to .csv

--- Scripts/outputPower.m
+++ Scripts/outputPower.m
@@ -1,1 +1,4 @@
-% Writes Power Band Values to an Excel Spreadsheet
+% Writes Power Band Values to a .csv file
+
+filename = 'powerData.csv';
+header = {'Window','Delta','Theta','Alpha','Beta','Gamma'};
