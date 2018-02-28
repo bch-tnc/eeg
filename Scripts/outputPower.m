bce3133a935add23f78cb0568e3e5b89156de309
Kenny <kenny.yau@tufts.edu>
2018-02-28 17:19:40 -0500

    adds ability to change to the test directory

--- Scripts/outputPower.m
+++ Scripts/outputPower.m
@@ -1,4 +1,15 @@
+%% outputPower
 % Writes Power Band Values to a .csv file
+
+cd ..
+cd Data
+
+% later, make outputPower work for multiple experiments
+% for now, the folder w/ all the necessary .mat files are in 1 folder
+listing = dir;
+currExp = listing(10).name;
+
+cd(currExp)
 
 filename = 'powerData.csv';
 header = {'Window','Delta','Theta','Alpha','Beta','Gamma'};
