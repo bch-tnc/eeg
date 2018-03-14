bc5dc28166ad52d82290374972df48ae5fe8d077
Kenny <kenny.yau@tufts.edu>
2018-03-14 12:42:02 -0400

    averages power values for same window and genotype numbers in an experiment

---
+++ Scripts/calcGroupAvg.m
@@ -0,0 +1,35 @@
+%% calcGroupAvg
+% calculates the power avgs amongst an experiment's mice
+
+scriptPath = pwd;
+cd ..
+cd Data
+
+% later, make the script automatically cycle through all experiments
+% for now, the script runs on a specific folder
+listing = dir;
+currExp = listing(10).name;
+
+cd(currExp)
+currExpPath = pwd;
+
+load('expData.mat')
+
+genotype = input('What genotype to average? (Enter a number)');
+window   = input('What window to average? (Enter a number)');
+
+numBands = length(expData(1).powerRatios);
+powerSum = zeros(1,numBands);
+numWin   = 0;
+for k = 1:length(expData)
+    currWin = expData(k).winNum;
+    currGeno = expData(k).genotype;
+    if currWin == window && currGeno == genotype
+        powerSum = powerSum + expData(k).powerRatios;
+        numWin = numWin + 1;
+    end
+end
+
+powerAvg = powerSum/numWin
+%% Return to Sender
+cd(scriptPath)
