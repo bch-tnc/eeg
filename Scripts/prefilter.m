c9b5e606acdf9ffa6e3e80a5448eb4db7114e745
Kenny <kenny.yau@tufts.edu>
2018-03-14 12:11:09 -0400

    adds overlay of ffts

--- Scripts/prefilter.m
+++ Scripts/prefilter.m
@@ -39,7 +39,8 @@ title('60Hz Notch Filter Using iirnotch')
 % Adapted from:
 % https://www.mathworks.com/matlabcentral/answers/361348-how-i-applly-a-bandpass-filter-in-a-signal
 % under answer by Star Strider
-N = 2.^nextpow2(length(trace_window));
+trace_copy = trace(:,2);
+N = 2.^nextpow2(length(trace_copy));
 
 Fs = 1000;                                                  % Sampling Frequency (Hz)
 Fn = Fs/2;                                                  % Nyquist Frequency (Hz)
@@ -55,13 +56,32 @@ figure
 freqz(sosbp,N,Fs)                                      % Filter Bode Plot
 title('Bandpass Filter Frequency Response')
 
-filtered_signal = filtfilt(sosbp, gbp, trace_window);    % Filter Signal
+trace_filtered = filtfilt(sosbp, gbp, trace_copy);    % Filter Signal
 
+% overlaying time signals
 figure
-plot(trace_window); hold on
-plot(filtered_signal)
-title('Trace Window Plots')
+plot(trace_copy); hold on
+plot(trace_filtered)
+title('Trace Plots')
 legend('Original','Bandpass Filtered')
+hold off 
 
+% overlaying both ffts
+figure
+N = 2.^nextpow2(length(trace(:,2)));
+f = (-N/2:N/2-1)*(Fs/N);
+TRACE = fftshift(fft(trace(:,2),N));
+plot(f,abs(TRACE))
+
+hold on
+N = 2.^nextpow2(length(trace_filtered));
+f = (-N/2:N/2-1)*(Fs/N);
+TRACE_filt = fftshift(fft(trace_filtered,N));
+plot(f,abs(TRACE_filt))
+title('FFT (Absolute Value)')
+xlabel('Frequency (Hz)')
+ylabel('Magnitude')
+xlim([0 100])
+legend('Original','Bandpass Filtered')
 
 cd(scriptPath)
