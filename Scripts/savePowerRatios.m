67be1dbaf7b9425fadcc638dc3091a9501330978
Kenny <kenny.yau@tufts.edu>
2018-05-09 11:38:20 -0400

    forgot to push the saving function to the repo

---
+++ Scripts/savePowerRatios.m
@@ -0,0 +1,43 @@
+function savePowerRatios(traceStruct,expPath,filename)
+% function savePowerRatios(powerRatios,savefile)
+% Saves powerRatio data to an Excel file
+
+cd(expPath)
+
+currMouse = traceStruct.mouse;
+currWin   = traceStruct.winNum;
+currGenotype = traceStruct.genotype;
+powerRatios = traceStruct.powerRatios;
+
+if nargin < 3
+    filename = 'powerData.csv';
+end
+header = {'Mouse','Window','Genotype','Delta','Theta','Alpha','Beta','Gamma'};
+
+% if listing.name == filename
+fid = fopen(filename,'a');
+if fid == -1, error('Cannot open file'); end
+
+% taken from
+% https://www.mathworks.com/matlabcentral/answers/70018-how-to-write-cell-array-into-a-csv-file
+% by Cedric Wannaz
+% writes entries in cell arrays to .csv file
+fprintf(fid, '%s,', header{1,1:end-1}) ;
+fprintf(fid, '%s\n', header{1,end}) ;
+fclose('all');
+
+% write label
+winText = {sprintf('%d',currMouse),sprintf('%d',currWin),sprintf('%d',currGenotype)};
+fid = fopen(filename,'a');
+if fid == -1, error('Cannot open file'); end
+
+fprintf(fid, '%s,', winText{1});
+fprintf(fid, '%s,', winText{2});
+fprintf(fid, '%s,', winText{3});
+fclose('all');
+
+% write values to .csv
+dlmwrite(filename,powerRatios,'precision',10,'-append','coffset',0);
+
+fclose('all');
+return
