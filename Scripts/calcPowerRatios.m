f1698ef7dbe23b3d7ef2f63466733078b288d509
Kenny <kenny.yau@tufts.edu>
2018-05-09 10:52:31 -0400

    comments out code that saves values to hard disk

--- Scripts/calcPowerRatios.m
+++ Scripts/calcPowerRatios.m
@@ -20,17 +20,17 @@ header = {'Mouse','Window','Genotype','Delta','Theta','Alpha','Beta','Gamma'};
 
 cd(expPath)
 
-% if listing.name == filename
-fid = fopen(filename,'a');
-if fid == -1, error('Cannot open file'); end
-
-% taken from
-% https://www.mathworks.com/matlabcentral/answers/70018-how-to-write-cell-array-into-a-csv-file
-% by Cedric Wannaz
-% writes entries in cell arrays to .csv file
-fprintf(fid, '%s,', header{1,1:end-1}) ;
-fprintf(fid, '%s\n', header{1,end}) ;
-fclose('all');
+% % if listing.name == filename
+% fid = fopen(filename,'a');
+% if fid == -1, error('Cannot open file'); end
+% 
+% % taken from
+% % https://www.mathworks.com/matlabcentral/answers/70018-how-to-write-cell-array-into-a-csv-file
+% % by Cedric Wannaz
+% % writes entries in cell arrays to .csv file
+% fprintf(fid, '%s,', header{1,1:end-1}) ;
+% fprintf(fid, '%s\n', header{1,end}) ;
+% fclose('all');
 
 currMouse    = traceStruct.mouse;
 currWin      = traceStruct.winNum;
@@ -53,18 +53,18 @@ cd(expPath)
 
 powerRatios = bandPowers/totalPower;
 
-% write label
-winText = {sprintf('%d',currMouse),sprintf('%d',currWin),sprintf('%d',currGenotype)};
-fid = fopen(filename,'a');
-if fid == -1, error('Cannot open file'); end
-
-fprintf(fid, '%s,', winText{1});
-fprintf(fid, '%s,', winText{2});
-fprintf(fid, '%s,', winText{3});
-fclose('all');
-
-% write values to .csv
-dlmwrite(filename,powerRatios,'precision',10,'-append','coffset',0);
+% % write label
+% winText = {sprintf('%d',currMouse),sprintf('%d',currWin),sprintf('%d',currGenotype)};
+% fid = fopen(filename,'a');
+% if fid == -1, error('Cannot open file'); end
+% 
+% fprintf(fid, '%s,', winText{1});
+% fprintf(fid, '%s,', winText{2});
+% fprintf(fid, '%s,', winText{3});
+% fclose('all');
+% 
+% % write values to .csv
+% dlmwrite(filename,powerRatios,'precision',10,'-append','coffset',0);
 
 % plot power ratios
 if isPlotted
@@ -82,5 +82,5 @@ if isPlotted
     xlim([0 numBands+1])
 end
 
-fclose('all');
+% fclose('all');
 return
